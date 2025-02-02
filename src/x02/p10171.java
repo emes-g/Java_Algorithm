9bc8a88c740d6cd8e12eb706ec93d43dc37189a0
Minseok <saltishDev@gmail.com>
2025-02-02 19:22:20 +0900

    10171: 고양이

---
+++ src/x02/p10171.java
@@ -0,0 +1,10 @@
+package x02;
+
+public class p10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
