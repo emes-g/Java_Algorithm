315a4ae3319f461a4a63f4ea3506391183b360f6
Minseok <saltishDev@gmail.com>
2025-02-14 16:46:02 +0900

    4179: 불! (시간 초과 해결)
    1. 시작점이 두 종류이면서, 쌍방이 아닌 단방향으로 영향을 미치는 경우
    2. 큐에 삽입되는 순서는 거리 순이다.

--- src/x09/p4179.java
+++ src/x09/p4179.java
@@ -12,7 +12,7 @@ public class p4179 {
     static final int[] dy = {0, 1, 0, -1};
 
     static int r, c;
-    static char[][] j, f;
+    static char[][] board;
     static int[][] jDist, fDist;
 
     public static void main(String[] args) throws IOException {
@@ -20,63 +20,24 @@ public class p4179 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
-        j = new char[r + 1][c + 1];
-        f = new char[r + 1][c + 1];
+        board = new char[r + 1][c + 1];
         jDist = new int[r + 1][c + 1];
         fDist = new int[r + 1][c + 1];
         for (int i = 1; i <= r; i++) {
             String str = br.readLine();
-            for (int k = 1; k <= c; k++) {
-                char c = str.charAt(k - 1);
-                j[i][k] = f[i][k] = c;
-                jDist[i][k] = fDist[i][k] = -1;
-                if (c == 'J') {
-                    f[i][k] = '.';
-                } else if (c == 'F') {
-                    j[i][k] = '#';
-                }
+            for (int j = 1; j <= c; j++) {
+                board[i][j] = str.charAt(j - 1);
+                jDist[i][j] = fDist[i][j] = -1;
             }
         }
-        bfs(j, jDist);
-        bfs(f, fDist);
 
-        int min = Integer.MAX_VALUE;
-        // 위, 아래
-        for (int i = 1; i <= r; i += (r - 1)) {
-            for (int k = 1; k <= c; k++) {
-                if (j[i][k] == '#' || jDist[i][k] == -1) {
-                    continue;
-                }
-                if (fDist[i][k] == -1 || jDist[i][k] < fDist[i][k]) {
-                    min = Math.min(min, jDist[i][k]);
-                }
-            }
-        }
-        // 왼쪽, 오른쪽
-        for (int i = 2; i <= r - 1; i++) {
-            for (int k = 1; k <= c; k += (c - 1)) {
-                if (j[i][k] == '#' || jDist[i][k] == -1) {
-                    continue;
-                }
-                if (fDist[i][k] == -1 || (jDist[i][k] < fDist[i][k])) {
-                    min = Math.min(min, jDist[i][k]);
-                }
-            }
-        }
-        if (min == Integer.MAX_VALUE) {
-            System.out.println("IMPOSSIBLE");
-            return;
-        }
-        System.out.println(min + 1);
-    }
-
-    public static void bfs(char[][] board, int[][] dist) {
+        // 불에 대한 BFS 먼저
         Queue<pair4179> q = new LinkedList<>();
         for (int i = 1; i <= r; i++) {
-            for (int k = 1; k <= c; k++) {
-                if (board[i][k] == 'J' || board[i][k] == 'F') {
-                    dist[i][k] = 0;
-                    q.offer(new pair4179(i, k));
+            for (int j = 1; j <= c; j++) {
+                if (board[i][j] == 'F') {
+                    fDist[i][j] = 0;
+                    q.offer(new pair4179(i, j));
                 }
             }
         }
@@ -88,13 +49,45 @@ public class p4179 {
                 if (nx < 1 || nx > r || ny < 1 || ny > c) {
                     continue;
                 }
-                if (board[nx][ny] == '#' || dist[nx][ny] != -1) {
+                if (board[nx][ny] == '#' || fDist[nx][ny] != -1) {
                     continue;
                 }
-                dist[nx][ny] = dist[curr.x][curr.y] + 1;
+                fDist[nx][ny] = fDist[curr.x][curr.y] + 1;
                 q.offer(new pair4179(nx, ny));
             }
         }
+
+        // 이후, 지훈이에 대한 BFS
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= c; j++) {
+                if (board[i][j] == 'J') {
+                    jDist[i][j] = 0;
+                    q.offer(new pair4179(i, j));
+                    i = r;
+                    break;
+                }
+            }
+        }
+        while (!q.isEmpty()) {
+            pair4179 curr = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = curr.x + dx[i];
+                int ny = curr.y + dy[i];
+                if (nx < 1 || nx > r || ny < 1 || ny > c) { // 지훈이가 맵 밖으로 탈출한 경우
+                    System.out.println(jDist[curr.x][curr.y] + 1);
+                    return;
+                }
+                if (board[nx][ny] == '#' || jDist[nx][ny] != -1) {
+                    continue;
+                }
+                if (fDist[nx][ny] != -1 && (fDist[nx][ny] <= jDist[curr.x][curr.y] + 1)) {  // 지훈이가 가기에 앞서, 불이 난 공간인 경우
+                    continue;
+                }
+                jDist[nx][ny] = jDist[curr.x][curr.y] + 1;
+                q.offer(new pair4179(nx, ny));
+            }
+        }
+        System.out.println("IMPOSSIBLE");
     }
 }
 
