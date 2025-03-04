2d1af6a64ab0312d5fd6ec03e73b1d8661a0e83c
Minseok <saltishDev@gmail.com>
2025-03-04 16:48:48 +0900

    1629: 곱셈 (시간 초과)

---
+++ src/x10/p1629.java
@@ -0,0 +1,29 @@
+package x10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class p1629 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        System.out.println(run(a, b, c));
+    }
+
+    public static long run(long a, long b, long c) {
+        if (b == 1) {
+            return a % c;
+        }
+        long val = run(a, b / 2, c);
+        if (b % 2 == 0) {
+            return val * val % c;
+        } else {
+            return run(a, b - 1, c) * a % c;
+        }
+    }
+}
