2c6ca933bc23fb8a0686286369322bf71c50378e
Minseok <saltishDev@gmail.com>
2025-03-11 18:13:26 +0900

    15657: N과 M (8)

---
+++ src/x0c/p15657.java
@@ -0,0 +1,43 @@
+package x0c;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class p15657 {
+    static int n, m;
+    static int[] num, arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        num = new int[n];
+        arr = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(num);
+        func(0, 0);
+        System.out.println(sb);
+    }
+
+    public static void func(int depth, int start) {
+        if (depth == m) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for (int i = start; i < n; i++) {
+            arr[depth] = num[i];
+            func(depth + 1, i);
+        }
+    }
+}
