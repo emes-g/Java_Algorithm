31d90159445b43874089ccc3c13deb5cd3621f42
Minseok <saltishDev@gmail.com>
2025-03-11 16:59:07 +0900

    15649: N과 M (1) (refactor)
    리스트 삽입/삭제하지 않고, 배열에 값 덮어쓰기

--- src/x0c/p15649.java
+++ src/x0c/p15649.java
@@ -3,13 +3,12 @@ package x0c;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class p15649 {
     static int n, m;
     static boolean[] check;
-    static LinkedList<Integer> list = new LinkedList<>();
+    static int[] arr;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] arg) throws IOException {
@@ -18,13 +17,14 @@ public class p15649 {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         check = new boolean[n + 1];
+        arr = new int[m];
         func(0);
         System.out.println(sb);
     }
 
     public static void func(int depth) {
         if (depth == m) {
-            for (Integer val : list) {
+            for (int val : arr) {
                 sb.append(val).append(' ');
             }
             sb.append('\n');
@@ -32,11 +32,10 @@ public class p15649 {
         }
         for (int i = 1; i <= n; i++) {
             if (!check[i]) {
+                arr[depth] = i;
                 check[i] = true;
-                list.add(i);
                 func(depth + 1);
                 check[i] = false;
-                list.removeLast();
             }
         }
     }
