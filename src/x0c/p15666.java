33b020305f94746f6a1e5f62f553a27921a7c574
Minseok <saltishDev@gmail.com>
2025-03-11 20:58:32 +0900

    15666: N과 M (12)

---
+++ src/x0c/p15666.java
@@ -0,0 +1,49 @@
+package x0c;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class p15666 {
+    static int n, m;
+    static int[] num, idx;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        num = new int[n];
+        idx = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(num);
+        func(0, 0);
+        System.out.println(sb);
+    }
+
+    public static void func(int depth, int start) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(num[idx[i]]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        int prev = 0;
+        for (int i = start; i < n; i++) {
+            int curr = num[i];
+            if (curr == prev) {
+                continue;
+            }
+            idx[depth] = i;
+            func(depth + 1, i);
+            prev = curr;
+        }
+    }
+}
