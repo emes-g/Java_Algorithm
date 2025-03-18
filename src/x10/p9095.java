2ac2abc6f03380336d84df19b49acdfff0eb517f
Minseok <saltishDev@gmail.com>
2025-03-18 16:16:57 +0900

    9095: 1, 2, 3 더하기

---
+++ src/x10/p9095.java
@@ -0,0 +1,27 @@
+package x10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p9095 {
+    static int n;
+    static int[] dp = new int[11];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            n = Integer.parseInt(br.readLine());
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
+            for (int i = 4; i <= n; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+            }
+            sb.append(dp[n]).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
