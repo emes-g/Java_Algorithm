0b697a9fe5a6c1cfdaaa43e0ed808463213e4755
Minseok <saltishDev@gmail.com>
2025-03-18 16:45:25 +0900

    9095: 1, 2, 3 더하기 (개선)
    TC마다 dp를 계산하지 않도록 수정

--- src/x10/p9095.java
+++ src/x10/p9095.java
@@ -11,15 +11,15 @@ public class p9095 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i < 11; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
         int t = Integer.parseInt(br.readLine());
         while (t-- > 0) {
             n = Integer.parseInt(br.readLine());
-            dp[1] = 1;
-            dp[2] = 2;
-            dp[3] = 4;
-            for (int i = 4; i <= n; i++) {
-                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-            }
             sb.append(dp[n]).append('\n');
         }
         System.out.println(sb);
