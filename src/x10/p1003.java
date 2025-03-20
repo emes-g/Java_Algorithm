e61af87845748813ef3407a7bf45a0da54b65ced
Minseok <saltishDev@gmail.com>
2025-03-20 15:37:52 +0900

    1003: 피보차니 함수 (개선)

--- src/x10/p1003.java
+++ src/x10/p1003.java
@@ -5,25 +5,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class p1003 {
-    static int[] dp = new int[41];
+    // dp[i][j] : fibonacci(i)를 호출했을 때, j가 출력되는 횟수
+    static int[][] dp = new int[41][2];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        dp[1] = 1;
-        dp[2] = 1;
-        for (int i = 3; i < 41; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+        for (int i = 2; i < 41; i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+            dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
         }
 
         StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
-            if (n == 0) {
-                sb.append("1 0\n");
-                continue;
-            }
-            sb.append(dp[n - 1]).append(' ').append(dp[n]).append('\n');
+            sb.append(dp[n][0]).append(' ').append(dp[n][1]).append('\n');
         }
         System.out.println(sb);
     }
