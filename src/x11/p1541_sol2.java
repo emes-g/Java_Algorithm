b2cabd75419f568117646486460a68093ac8c457
Minseok <saltishDev@gmail.com>
2025-04-10 21:26:35 +0900

    1541: 잃어버린 괄호 (참고 풀이)

--- src/x11/p1541_sol2.java
+++ src/x11/p1541_sol2.java
@@ -1,2 +1,31 @@
-package x11;public class p1541_sol2 {
+package x11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class p1541_sol2 {
+    // 덧셈을 먼저 계산하고, (부분합을 먼저 생성해주고)
+    // 간편하게 빼버리는 방식
+    // ex.
+    // 30-70-20+40-10+100+30-35
+    // 30-(70)-(20+40)-(10+100+30)-35
+    static int sum;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), "-");
+        String[] nums = st.nextToken().split("[+]");
+        for (String num : nums) {
+            sum += Integer.parseInt(num);
+        }
+        while (st.hasMoreTokens()) {
+            nums = st.nextToken().split("[+]");
+            for (String num : nums) {
+                sum -= Integer.parseInt(num);
+            }
+        }
+        System.out.println(sum);
+    }
 }
