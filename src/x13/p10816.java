39f6a2aa8511b258e304862346b617428e985a5b
Minseok <saltishDev@gmail.com>
2025-04-19 15:47:01 +0900

    10816: 숫자 카드 2 (상하한 기본)

---
+++ src/x13/p10816.java
@@ -0,0 +1,60 @@
+package x13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class p10816 {
+    static int n, m;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);   // O(nlogn)
+        m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        while (m-- > 0) {   // O(mlogn)
+            int target = Integer.parseInt(st.nextToken());
+            int cnt = upperBound(target) - lowerBound(target);
+            sb.append(cnt).append(' ');
+        }
+        System.out.println(sb);
+    }
+
+    public static int lowerBound(int target) {
+        int start = 0;
+        int end = arr.length;
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] >= target) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+
+    public static int upperBound(int target) {
+        int start = 0;
+        int end = arr.length;
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] > target) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
