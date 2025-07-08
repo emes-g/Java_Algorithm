457d4922c4f888280eb8332659c91678187f86b1
Minseok <saltishDev@gmail.com>
2025-07-08 14:18:22 +0900

    14938: 서강그라운드

---
+++ src/x1c/p14938.java
@@ -0,0 +1,56 @@
+package x1c;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class p14938 {
+    static final int MAX = Integer.MAX_VALUE / 2;
+    static int n, m, r;
+    static int[] items;
+    static int[][] dist;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        items = new int[n + 1];
+        dist = new int[n + 1][n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            items[i] = Integer.parseInt(st.nextToken());
+            Arrays.fill(dist[i], MAX);
+            dist[i][i] = 0;
+        }
+        for (int i = 0; i < r; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            dist[from][to] = dist[to][from] = weight;
+        }
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            for (int j = 1; j <= n; j++) {
+                if (dist[i][j] > m) {
+                    continue;
+                }
+                sum += items[j];
+            }
+            ans = Math.max(ans, sum);
+        }
+        System.out.println(ans);
+    }
+}
