671ce83d7d3784c4929ebc0bddf3ec90264b8892
Minseok <saltishDev@gmail.com>
2026-01-26 20:47:48 +0900

    15650: N과 M (2) (오름차순 순열)

--- src/x0c/p15650.java
+++ src/x0c/p15650.java
@@ -1,42 +1,46 @@
 package x0c;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/*
+오름차순 순열: 기본 순열(nPm)에 시작 인덱스(start)만 추가
+ */
+
 public class p15650 {
     static int n, m;
-    static boolean[] check;
     static int[] arr;
+    static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        check = new boolean[n + 1];
         arr = new int[m];
+        visited = new boolean[n + 1];   // 1-index
         func(0, 1);
         System.out.println(sb);
     }
 
-    public static void func(int depth, int start) {
-        if (depth == m) {
-            for (int val : arr) {
-                sb.append(val).append(' ');
+    public static void func(int level, int start) {
+        if (level == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(' ');
             }
             sb.append('\n');
             return;
         }
         for (int i = start; i <= n; i++) {
-            if (!check[i]) {
-                arr[depth] = i;
-                check[i] = true;
-                func(depth + 1, i);
-                check[i] = false;
+            if (visited[i]) {
+                continue;
             }
+            visited[i] = true;
+            arr[level] = i;
+            func(level + 1, i + 1);
+            visited[i] = false;
         }
     }
 }
