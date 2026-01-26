b7318e58cf0e7019d7c593d56a3a38b9cb316fca
Minseok <saltishDev@gmail.com>
2026-01-26 20:47:19 +0900

    15649: N과 M (1) (순열)

--- src/x0c/p15649.java
+++ src/x0c/p15649.java
@@ -1,42 +1,44 @@
 package x0c;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 순열 (nPm)
+
 public class p15649 {
     static int n, m;
-    static boolean[] check;
     static int[] arr;
+    static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] arg) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        check = new boolean[n + 1];
         arr = new int[m];
+        visited = new boolean[n + 1];   // 1-index
         func(0);
         System.out.println(sb);
     }
 
-    public static void func(int depth) {
-        if (depth == m) {
-            for (int val : arr) {
-                sb.append(val).append(' ');
+    public static void func(int level) {
+        if (level == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(' ');
             }
             sb.append('\n');
             return;
         }
         for (int i = 1; i <= n; i++) {
-            if (!check[i]) {
-                arr[depth] = i;
-                check[i] = true;
-                func(depth + 1);
-                check[i] = false;
+            if (visited[i]) {
+                continue;
             }
+            visited[i] = true;
+            arr[level] = i;
+            func(level + 1);
+            visited[i] = false;
         }
     }
 }
