1ec62cdfd587087ac2befa5f6b160c889f115d83
Minseok <saltishDev@gmail.com>
2026-01-26 20:48:29 +0900

    15651: N과 M (3) (중복 순열)

--- src/x0c/p15651.java
+++ src/x0c/p15651.java
@@ -1,16 +1,17 @@
 package x0c;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+
+// 중복 순열 (nπm)
 
 public class p15651 {
     static int n, m;
     static int[] arr;
     static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
@@ -20,17 +21,17 @@ public class p15651 {
         System.out.println(sb);
     }
 
-    public static void func(int depth) {
-        if (depth == m) {
-            for (int val : arr) {
-                sb.append(val).append(' ');
+    public static void func(int level) {
+        if (level == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(' ');
             }
             sb.append('\n');
             return;
         }
         for (int i = 1; i <= n; i++) {
-            arr[depth] = i;
-            func(depth + 1);
+            arr[level] = i;
+            func(level + 1);
         }
     }
 }
