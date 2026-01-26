254dc2f30d2ecdf31173cb92fbdd93ec186fdcc6
Minseok <saltishDev@gmail.com>
2026-01-26 20:49:25 +0900

    15652: N과 M (4) (비내림차순 중복 순열)

--- src/x0c/p15652.java
+++ src/x0c/p15652.java
@@ -1,16 +1,17 @@
 package x0c;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+
+// 비내림차순 중복 순열: 중복 순열에 시작 인덱스(start)만 추가
 
 public class p15652 {
     static int n, m;
     static int[] arr;
     static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
@@ -20,17 +21,17 @@ public class p15652 {
         System.out.println(sb);
     }
 
-    public static void func(int depth, int start) {
-        if (depth == m) {
-            for (int val : arr) {
-                sb.append(val).append(' ');
+    public static void func(int level, int start) {
+        if (level == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(' ');
             }
             sb.append('\n');
             return;
         }
         for (int i = start; i <= n; i++) {
-            arr[depth] = i;
-            func(depth + 1, i);
+            arr[level] = i;
+            func(level + 1, i);
         }
     }
 }
